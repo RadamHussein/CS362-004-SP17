8172fb1e5c4f01f39a6f7cd7644370a144d2465b
Adam Smith <stupidalphabet@gmail.com>
2017-04-16 01:01:48 -0500

    commiting initial code changes

--- projects/smithada/dominion/dominion_helpers.h
+++ projects/smithada/dominion/dominion_helpers.h
@@ -12,4 +12,11 @@ int getCost(int cardNumber);
 int cardEffect(int card, int choice1, int choice2, int choice3, 
 	       struct gameState *state, int handPos, int *bonus);
 
+//5 Card Functions
+void smithyFunction(int currentPlayer, int handPos, struct gameState *state);
+void adventurerFunction(int currentPlayer, struct gameState *state, int drawntreasure);
+int mineFunction(int currentPlayer, int choice1, int choice2, struct gameState *state, int handPos);
+void minionFunction(int currentPlayer, int choice1, int choice2, struct gameState *state, int handPos);
+int treasuremapFunction(int currentPlayer, struct gameState *state, int handPos);
+
 #endif
